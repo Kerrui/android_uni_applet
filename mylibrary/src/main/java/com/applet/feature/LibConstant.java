91e79fb7a326d6c42cd7af45d4035b642c0020e0
genda <gendaliu@yeah.net>
2023-09-26 17:03:10 +0800

    release

--- mylibrary/src/main/java/com/applet/feature/LibConstant.java
+++ mylibrary/src/main/java/com/applet/feature/LibConstant.java
@@ -2,7 +2,7 @@ package com.applet.feature;
 
 public class LibConstant {
 
-    private static final String env = "dev";
+    private static final String env = "res";
     public static final String SDK_VERSION = "1.0.0";
 
     public static final String SP_DIRECT_OPEN = "sp_direct_open";
@@ -11,12 +11,10 @@ public class LibConstant {
     public static final String SP_WGT_APPLET = "sp_wgt_applet";
 
     public static String getHost() {
-        if (isDev()) return "https://bts.267girl.com";
         return "https://service.nbcustomchat.com";
     }
 
     public static String getApiK() {
-        if (isDev()) return "1b28c2eac0672761";
         return "907fc66d911eadd8";
     }
 
