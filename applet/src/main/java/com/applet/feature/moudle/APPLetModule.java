37a0d5d8579c176f3103fb51f9c47f2a65e625c1
leechli <leechli@hotmail.com>
2024-12-19 15:43:14 +0800

    feat(applet): 添加客户数据获取功能并优化环境配置
    - 新增 uniBasePlatform 方法，返回平台类型
    - 实现 getCustomerData 方法，用于获取客户数据
    - 优化环境配置，移除 getHost 方法
    - 重构 SP_UID 和 DEFAULT_APPLET_ID 常量定义

--- applet/src/main/java/com/applet/feature/moudle/APPLetModule.java
+++ applet/src/main/java/com/applet/feature/moudle/APPLetModule.java
@@ -64,7 +64,6 @@ public class APPLetModule extends UniModule {
     }
 
 
-
     @Deprecated
     @UniJSMethod(uiThread = true)
     public void close(JSONObject jsonObject) {
@@ -80,8 +79,10 @@ public class APPLetModule extends UniModule {
         }
     }
 
-
-
+    @UniJSMethod(uiThread = true)
+    public int uniBasePlatform() {
+        return 0;
+    }
 
     @UniJSMethod(uiThread = false)
     public boolean putPackageData(JSONObject params) {
@@ -107,6 +108,8 @@ public class APPLetModule extends UniModule {
     public static void open(Context context, JSONObject jsonObject, DCUniMPJSCallback callback) {
         JSONObject ret = new JSONObject();
         String path = jsonObject.getString("path");
+
+
         File wgt = new File(path);
         if (!wgt.exists() || !wgt.isFile()) {
             ret.put("succeed", false);
@@ -115,6 +118,11 @@ public class APPLetModule extends UniModule {
             return;
         }
         String appid = jsonObject.getString("appid");
+        String channelId = jsonObject.getString("channel_id");
+        JSONObject info = jsonObject.getJSONObject("info");
+
+        MMKVUtil.getInstance().saveJSONObject(channelId, info);
+
         UniManager.releaseWgtToRunPath(path, appid, new UniManager.IOnWgtReleaseListener() {
             @Override
             public void onSuccess() {
@@ -146,6 +154,66 @@ public class APPLetModule extends UniModule {
             }
         });
     }
+
+    @UniJSMethod(uiThread = false)
+    public JSONObject getCustomerData(JSONObject params) {
+        String spKey;
+        spKey = params.containsKey("name") ? params.getString("name") : "";
+        if (TextUtils.isEmpty(spKey)) {
+            spKey = MMKVUtil.getInstance().getString("aName", "");
+        }
+        String dataJsonStr = MMKVUtil.getInstance().getString(spKey, "");
+        if (TextUtils.isEmpty(spKey) || TextUtils.isEmpty(dataJsonStr)) {
+            JSONObject info = new JSONObject();
+            info.put("page_home", "https://xxx.com");
+            info.put("bucket", "https://xxx.com/");
+            info.put("bucket_pic", "https://img.chatapp.com/");
+            info.put("batch", "wb");
+            info.put("split_line_bound", "+NUUoa/cyd");
+            info.put("split_line_random", "0148cc1cc133717cde5");
+            JSONObject push = new JSONObject();
+            push.put("token", "a");
+            push.put("active", "b");
+            push.put("login", "c");
+            push.put("register", "d");
+            push.put("pay", "e");
+            push.put("payLtvHigh", "f");
+            push.put("freeCallComplete", "g");
+            JSONObject result = new JSONObject();
+            //result.put("info", info);
+            //result.put("push", push);
+            return result;
+        }
+
+        JSONObject result = new JSONObject();
+        JSONObject dataObj = JSON.parseObject(dataJsonStr);
+        JSONObject dataInfoObj = dataObj.getJSONObject("info");
+        JSONObject dataPushObj = dataObj.containsKey("push") ? dataObj.getJSONObject("push") : null;
+
+        JSONObject info = new JSONObject();
+        info.put("page_home", dataInfoObj.getString("page_home"));
+        info.put("bucket", dataInfoObj.getString("bucket"));
+        info.put("bucket_pic", dataInfoObj.getString("bucket_pic"));
+        info.put("batch", dataInfoObj.getString("batch"));
+        info.put("split_line_bound", dataInfoObj.getString("split_line_bound"));
+        info.put("split_line_random", dataInfoObj.getString("split_line_random"));
+        result.put("info", info);
+
+        if (dataPushObj != null) {
+            JSONObject push = new JSONObject();
+            push.put("token", dataPushObj.getString("token"));
+            push.put("active", dataPushObj.getString("active"));
+            push.put("login", dataPushObj.getString("login"));
+            push.put("register", dataPushObj.getString("register"));
+            push.put("pay", dataPushObj.getString("pay"));
+            push.put("payLtvHigh", dataPushObj.getString("payLtvHigh"));
+            push.put("freeCallComplete", dataPushObj.getString("freeCallComplete"));
+            result.put("push", push);
+        }
+
+        return result;
+    }
+
 
     public static void initCallback(Context context) {
         DCUniMPSDK.getInstance().setUniMPOnCloseCallBack(s -> MPStack.getInstance().remove(s));
