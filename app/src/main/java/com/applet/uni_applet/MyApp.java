7764d0f3f3952c0500845916defb641deeefe5ae
leechli <leechli@hotmail.com>
2025-01-07 20:32:47 +0800

    refactor(applet): 移除未使用的代码并简化初始化流程- 删除了 MyApp 中未使用的导入和冗余代码 - 移除了 AppSigning 和 LibApp 类，这些类在当前代码中未被使用 - 简化了 PluginInit 的初始化流程

--- app/src/main/java/com/applet/uni_applet/MyApp.java
+++ app/src/main/java/com/applet/uni_applet/MyApp.java
@@ -1,18 +1,11 @@
 package com.applet.uni_applet;
 
 
-import android.app.Activity;
-import android.content.Intent;
-
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
 import com.applet.feature.AppLibSdk;
 import com.applet.feature.OnAppLibInitializeListener;
 import com.hi.chat.uniplugin.ApplicationListener;
 import com.hi.chat.uniplugin.PluginInit;
 import com.hi.chat.uniplugin.feature.util.LogUtil;
-import com.hi.chat.uniplugin_mqtt.event.MyFirebaseMessagingService;
-import com.hi.chat.uniplugin_tool.ToolUtil;
 
 import io.dcloud.application.DCloudApplication;
 
@@ -25,9 +18,7 @@ public class MyApp extends DCloudApplication {
     public void onCreate() {
         super.onCreate();
 
-        PluginInit pluginInit = new PluginInit.Builder(this)
-                .build();
-        pluginInit.init();
+        new PluginInit.Builder(this).build().init();
         AppLibSdk.getInstance().initEngine(this, new OnAppLibInitializeListener() {
             @Override
             public void onInitFinished(boolean success) {
@@ -36,20 +27,6 @@ public class MyApp extends DCloudApplication {
         });
 
 
-        registerActivityLifecycleCallbacks(new ApplicationListener(new ApplicationListener.IMyApplicationListener() {
-            @Override
-            public void onActivityChange(Activity activity) {
-            }
-
-            @Override
-            public void onAppForegroundChange(boolean isForeground) {
-                MyFirebaseMessagingService.sIsAppForeground = isForeground;
-                Intent intent = new Intent(ToolUtil.getIntentActionApplication(MyApp.this));
-                intent.putExtra("action", isForeground ? 3 : 1);
-                sendBroadcast(intent);
-            }
-        }));
-
-
+        registerActivityLifecycleCallbacks(new ApplicationListener());
     }
 }
